81dca75fc0c7a8b2180ffaa7f56d27f4970ffc88
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2022-06-16 15:46:54 +0600

    metamask connected

--- src/components/drawer/WalletDrawer.tsx
+++ src/components/drawer/WalletDrawer.tsx
@@ -1,6 +1,10 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment } from 'react';
+import { UserIcon } from '@heroicons/react/solid';
+import Image from 'next/image';
+import React, { Fragment, useContext, useState } from 'react';
+
+import { WalletContext } from '@/contexts/walletContext';
 
 //set type for open and setOpen
 interface TypeSetOpen {
@@ -9,6 +13,90 @@ interface TypeSetOpen {
 }
 
 export default function WalletDrawer({ open, setOpen }: TypeSetOpen) {
+  const { wallet, setWalletAddress } = useContext(WalletContext);
+  // my wallet states
+  const [_loading, setLoading] = useState(false);
+  const [_message, setMessage] = useState('');
+  //const [account, setAccount] = useState(null);
+  const openMetamask = async () => {
+    setLoading(true);
+    const { ethereum } = window;
+
+    const accounts = await ethereum.request({
+      method: 'wallet_requestPermissions',
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+
+    if (ethereum?.networkVersion !== '13881') {
+      const polygon = {
+        chainId: `0x${Number(137).toString(16)}`,
+        chainName: 'Polygon Mainnet',
+        nativeCurrency: {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18,
+        },
+        rpcUrls: ['https://polygon-rpc.com/'],
+        blockExplorerUrls: ['https://polygonscan.com/'],
+      };
+
+      try {
+        await window.ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              ...polygon,
+            },
+          ],
+        });
+
+        //set the wallet address to local storage
+        localStorage.setItem('GraziaMetamask', 'true');
+        setWalletAddress(accounts[0].caveats[0].value[0]);
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/wallets/add`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              walletaddress: accounts[0].caveats[0].value[0],
+            }),
+          }
+        );
+
+        await response.json();
+
+        //console.log(wallet_add);
+
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        return setMessage('Failed to change network');
+      }
+    } else {
+      localStorage.setItem('GraziaMetamask', 'true');
+      setWalletAddress(accounts[0].caveats[0].value[0]);
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/wallets/add`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          walletaddress: accounts[0].caveats[0].value[0],
+        }),
+      });
+    }
+
+    setLoading(false);
+  };
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as='div' className='relative z-10' onClose={setOpen}>
@@ -60,18 +148,41 @@ export default function WalletDrawer({ open, setOpen }: TypeSetOpen) {
                   <div className='flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl'>
                     <div className='px-4 sm:px-6'>
                       <Dialog.Title className='text-lg font-medium text-gray-900'>
-                        {' '}
-                        Panel title{' '}
+                        <div className='group mt-3 inline-block rounded-full border bg-gray-900 '>
+                          <UserIcon className='w-6 text-white' />
+                        </div>
+                        <span className='p-3 text-lg font-medium text-gray-900'>
+                          {wallet.walletaddress ? (
+                            <>{wallet.walletaddress}</>
+                          ) : (
+                            <>My Wallet</>
+                          )}
+                        </span>
                       </Dialog.Title>
                     </div>
                     <div className='relative mt-6 flex-1 px-4 sm:px-6'>
                       {/* Replace with your content */}
-                      <div className='absolute inset-0 px-4 sm:px-6'>
-                        <div
-                          className='h-full border-2 border-dashed border-gray-200'
-                          aria-hidden='true'
+                      <h4 className='text-md'>
+                        {' '}
+                        Connect with one of our available wallet providers or
+                        create a new one.
+                      </h4>
+
+                      {/* table of wallet list */}
+                      <div
+                        onClick={openMetamask}
+                        className='mt-3 cursor-pointer border border-dashed border-slate-300 p-3'
+                      >
+                        <Image
+                          src='/images/logos/metamask-fox.svg'
+                          alt='Metamask Logo'
+                          width='25'
+                          height='25'
+                          className='relative -mb-3'
                         />
+                        <p className='relative -mt-8 ml-9'>Metamask</p>
                       </div>
+
                       {/* /End replace */}
                     </div>
                   </div>
--- src/components/menu/items/UserMenuItem.tsx
+++ src/components/menu/items/UserMenuItem.tsx
@@ -1,10 +1,10 @@
-import { UserIcon } from '@heroicons/react/solid';
+import { UserIcon } from '@heroicons/react/outline';
 
 const UserMenuItem = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center'>
-      <div className='group inline-block'>
-        <UserIcon className='w-7  rounded-full border bg-slate-400 text-white' />
+      <div className='group inline-block rounded-full border '>
+        <UserIcon className='w-7 text-slate-400' />
       </div>
     </div>
   );
